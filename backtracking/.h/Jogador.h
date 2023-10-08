52e458816bf9415c6094a59d32bba65570a571a8
Matheus Nogueira <95196338+MatheusNogueiraUfv@users.noreply.github.com>
2023-10-08 16:36:55 -0300

    Update Jogador.h

--- backtracking/.h/Jogador.h
+++ backtracking/.h/Jogador.h
@@ -1,4 +1,5 @@
 #include "C:\Users\mathe\Documents\CC++\C\Proprios\Trabalhos_PAA\TP1\.h\FunctionsArquivo.h"
+#include "C:\Users\mathe\Documents\CC++\C\Proprios\Trabalhos_PAA\TP1\.h\Pilha.h"
 #include <stdbool.h>
 typedef struct JOGADOR{
     int ChavesColetadas,JogadorCollun,JogadorLine,PercursoJogadorLine,PercursoJogadorCollun;
@@ -6,11 +7,13 @@ typedef struct JOGADOR{
 }JOGADOR;
 
 void InicializaJogador(JOGADOR *PtrJogador,MATRIZTABULEIRO *PtrTabuleiro);
-void movimentar(JOGADOR *PtrJogador,MATRIZTABULEIRO *PtrTabuleiro);
-void backtraking(int *i,int *j,JOGADOR *PtrJogador,MATRIZTABULEIRO *PtrTabuleiro);
+void movimentar(JOGADOR *PtrJogador,MATRIZTABULEIRO *PtrTabuleiro,PilhaCoordenadas *PtrPilha);
+int backtraking(int *control,int *i,int *j,JOGADOR *PtrJogador,MATRIZTABULEIRO *PtrTabuleiro,PilhaCoordenadas *PtrPilha);
 int Direita(int i,int j,JOGADOR *PtrJogador);
 int Esquerda(int i,int j,JOGADOR *PtrJogador);
 int Cima(int i,int j,JOGADOR *PtrJogador);
 int Baixo(int i,int j,JOGADOR *PtrJogador);
-int Retornar(int i,int j,JOGADOR *PtrJogador);
-int SemCaminho(int i,int j,JOGADOR *PtrJogador);
+int VoltarDireita(int i,int j,JOGADOR *PtrJogador);
+int VoltarEsquerda(int i,int j,JOGADOR *PtrJogador);
+int VoltarCima(int i,int j,JOGADOR *PtrJogador);
+int VoltarBaixo(int i,int j,JOGADOR *PtrJogador);
